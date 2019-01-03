c58814914f319ea1572cc14a0154b8cd2c2ef6a7
stefano benco potato <stefanobenco@brandatastudio.com>
2019-01-03 17:23:23 +0000

    First commit

---
+++ instalationfile.sh
@@ -0,0 +1,9 @@
+ 
+# basic instalation commands https://pypi.org/project/Wikipedia-API/
+*help.txt*	For Vim version 8.0.  Last change: 2017 Oct 28
+
+pip install wikipedia
+pip install numpy
+pip install pandas
+
+
